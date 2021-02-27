927c912125406cd261f1b2e5a25995c25cd32773
Daniel Sieradski <daniel@self.agency>
2021-02-27 02:03:06 -0500

    fixup

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 class StrapiMarkdown {
-  constructor(model, types, options) {
-    this.types = {}
+  constructor(
+    model,
+    types = {
+      standard: ['richtext'],
+      inline: ['string']
+    },
+    options = {
+      smartypants: true,
+      headerIds: false,
+      breaks: true
+    }
+  ) {
+    if (types && (types.constructor === Object || Array.isArray(types))) {
+      this.types = types
+    }
 
-    if (types && types.constructor === Object) {
-      this.types.standard = types.standard
-      this.types.inline = types.inline
-    } else if (types && Array.isArray(types)) {
-      this.types.standard = types
+    if (model) {
+      this.model = this.model.attributes
     } else {
-      this.types.standard = ['richtext']
-      this.types.inline = ['string']
+      throw new Error('StrapiMarkdown requires `model`')
     }
 
     this.marked = require('marked')
-
-    options
-      ? this.marked.setOptions(options)
-      : this.marked.setOptions({
-          smartypants: true,
-          headerIds: false,
-          breaks: true
-        })
+    this.marked.setOptions(options)
   }
 
   parse(data) {
     for (let key in this.model) {
-      let out
-
-      if (this.types.standard.includes(this.model[key].type)) {
-        out = this.marked(data[key] || '')
+      if (this.types.standard && this.types.standard.includes(this.model[key].type)) {
+        const out = this.marked(data[key] || '')
         data[key] = out.length ? out : null
-      } else if (this.types.inline.includes(this.model[key].type)) {
-        out = this.marked.parseInline(data[key] || '')
+      } else if (this.types.inline && this.types.inline.includes(this.model[key].type)) {
+        const out = this.marked.parseInline(data[key] || '')
         data[key] = out.length ? out : null
       }
     }
@@ -40,8 +40,6 @@ class StrapiMarkdown {
   }
 
   md(data) {
-    model = model.attributes
-
     if (Array.isArray(data)) {
       return data.map(obj => this.parse(obj))
     } else {
