75c3bd60d4a1f019d99e214e710429fc0e7ed350
Daniel Sieradski <daniel@self.agency>
2021-02-28 17:16:46 -0500

    improved

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-class StrapiMarkdown {
-  constructor(
-    model,
-    types = {
-      standard: ['richtext'],
-      inline: ['string']
-    },
-    options = {
-      smartypants: true,
-      headerIds: false,
-      breaks: true
-    }
-  ) {
-    if (types && types.constructor === Object) {
-      this.types = types
-    } else if (types && Array.isArray(types)) {
-      this.types = { standard: types }
-    } else {
-      throw new Error('`types` must be object or array')
-    }
+const defaults = {
+  types: {
+    standard: ['richtext'],
+    inline: ['string']
+  },
+  options: {
+    smartypants: true,
+    headerIds: false,
+    breaks: true
+  }
+}
 
-    if (model && model.constructor === Object) {
+class StrapiMarkdown {
+  constructor(model, types = defaults.types, options = defaults.options) {
+    if (model && model.attributes) {
       this.model = model.attributes
     } else {
-      throw new Error('`model` must be object')
+      throw new Error('`model` must be valid model object')
+    }
+
+    if (types && types.standard && Array.isArray(types.standard) && types.inline && Array.isArray(types.inline)) {
+      this.types = types
+    } else {
+      throw new Error('`types` must be object containing `standard` and `inline` arrays')
     }
 
     if (options && options.constructor === Object) {
       this.marked = require('marked')
       this.marked.setOptions(options)
     } else {
-      throw new Error('`options` must be object')
+      throw new Error('`options` must be valid object')
     }
   }
 
